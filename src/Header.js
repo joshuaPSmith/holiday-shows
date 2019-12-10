ab09b2a520a6ae2053350b026bf49626431eef6f
Josh Smith <joshuap.smith12@gmail.com>
2019-12-10 09:11:38 -0600

    Added more meta tags

--- src/Header.js
+++ src/Header.js
@@ -1,7 +1,13 @@
 import React from "react";
 import { Helmet } from "react-helmet";
+import fbLogo from "./assets/img/fb-pic.jpg";
 
 export default function Header() {
+  const title = "Holiday Show Finder!";
+  const description = `Use our app to find holiday episodes of The Office, Friends, 
+  Psych, Community, and Parks and recreation! This app will make it easy to get in the holiday season 
+  by showing you the right episodes for the holiday!`;
+  const url = "https://www.holidayshowfinder.com";
   return (
     <div>
       <Helmet>
@@ -9,12 +15,15 @@ export default function Header() {
           Holiday Show Finder - The easiest way to find holiday episodes of your
           favorite shows!
         </title>
-        <meta
-          name="description"
-          content="Use our app to find holiday episodes of The Office, Friends, 
-Psych, Community, and Parks and recreation! This app will make it easy to get in the holiday season 
-by showing you the right episodes for the holiday!"
-        />
+        <meta name="description" content={description} />
+        <meta name="og:url" content={url} />
+        <meta name="og:title" content={title} />
+        <meta name="og:description" content={description} />
+        <meta name="og:image" content={fbLogo} />
+        <meta name="og:type" content="website" />
+        <meta name="twitter:title" content={title} />
+        <meta name="twitter:description" content={description} />
+        <meta name="twitter:image" content={fbLogo} />
       </Helmet>
     </div>
   );
