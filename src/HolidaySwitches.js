564de1f6318ed84cb48607257ded6731efc884d8
Josh Smith <joshuap.smith12@gmail.com>
2020-01-01 21:38:42 -0600

    Working on some testing

--- src/HolidaySwitches.js
+++ src/HolidaySwitches.js
@@ -1,55 +1,41 @@
-import React from 'react';
-import FormGroup from '@material-ui/core/FormGroup';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
-import './css/main.css';
-import { withStyles } from '@material-ui/core/styles';
+import React from "react";
+import FormGroup from "@material-ui/core/FormGroup";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Checkbox from "@material-ui/core/Checkbox";
+import "./css/main.css";
+import { withStyles } from "@material-ui/core/styles";
 
 export default function HolidaySwitches(props) {
-  // const [state, setState] = React.useState({
-  //   christmas: true,
-  //   halloween: true,
-  //   thanksgiving: true,
-  //   valentinesDay: true,
-  // });
-
   const handleChange = name => event => {
-    props.switchChange(event.target.checked, name)
+    props.switchChange(event.target.checked, name);
   };
 
   const PinkCheckbox = withStyles({
     root: {
       color: "#ea4e7b",
-      '&$checked': {
+      "&$checked": {
         color: "#E21B54"
-      },
+      }
     },
-    checked: {},
+    checked: {}
   })(props => <Checkbox color="default" {...props} />);
 
   return (
-    // <FormGroup>
-    //   <FormControlLabel
-    //     control={
-    //       <Switch checked={props.toggleAllOn} onChange={toggleAllChange} />
-    //     }
-    //     label="Toggle All"
-    //   />
-    // </FormGroup>
-
     <FormGroup>
-      {
-        Object.keys(props.holidays).map((holiday, index) => {
-          return (
-            <FormControlLabel key={index}
-              control={
-                <PinkCheckbox checked={props.switchState[holiday]} onChange={handleChange(holiday)} />
-              }
-              label={props.holidays[holiday].name}
-            />
-          )
-        })
-      }
-    </FormGroup >
+      {Object.keys(props.holidays).map((holiday, index) => {
+        return (
+          <FormControlLabel
+            key={index}
+            control={
+              <PinkCheckbox
+                checked={props.switchState[holiday]}
+                onChange={handleChange(holiday)}
+              />
+            }
+            label={props.holidays[holiday].name}
+          />
+        );
+      })}
+    </FormGroup>
   );
 }
